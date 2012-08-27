5b1c3b5c3371c10681934726a30bb85cfb860738
Zeh Rizzatti <zehrizzatti@gmail.com>
2012-08-27 16:03:06 -0400

    add HEAD for rabbitmqc

--- rabbitmqc.rb
+++ rabbitmqc.rb
@@ -5,11 +5,13 @@ class Rabbitmqc < Formula
   url 'https://github.com/alanxz/rabbitmq-c/tarball/0.2'
   version '0.2'
   sha1 '37029aa53d542ddc2a529fbb7c1d2edb7ad007c0'
+  head 'https://github.com/alanxz/rabbitmq-c.git'
 
   depends_on 'cmake'
 
   def install
-    system "cmake", ".", '-DFETCH_CODEGEN_FROM_GIT=1', *std_cmake_args
+    std_cmake_args << '-DFETCH_CODEGEN_FROM_GIT=1' unless build.head?
+    system "cmake", ".", *std_cmake_args
     system "make"
     system "make install"
   end
