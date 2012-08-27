717462f3b3ca2ca354b33d367585b00631b1fd26
Zeh Rizzatti <zehrizzatti@gmail.com>
2012-08-27 16:11:46 -0400

    add HEAD for pjproject

--- pjproject.rb
+++ pjproject.rb
@@ -5,6 +5,7 @@ class Pjproject < Formula
   url 'http://www.pjsip.org/release/2.0.1/pjproject-2.0.1.tar.bz2'
   version '2.0.1'
   sha1 '67f24ae4ec781e20fe6111b64e08ef0e21f300b3'
+  head 'http://svn.pjsip.org/repos/pjproject/trunk'
 
   fails_with :clang do
     build 421
